032077855ca885f9a6ab4480e24be2773b215d45
Brian Martin <brayniac@gmail.com>
2015-10-24 12:41:24 -0700

    improvements
    - functions now take time, makes testing possible
    - tick() is simplified
    - add take() which returns a timespec for sleep
    - block() now uses take()
    - operate on quantum tokens at a time, reduces overhead
    - fmt::Debug for timespec
    - tests

--- src/lib.rs
+++ src/lib.rs
@@ -7,22 +7,32 @@
 extern crate time;
 
 use std::sync::mpsc;
+use std::fmt;
+
+const ONE_SECOND: u64 = 1_000_000_000;
 
 pub struct Ratelimit {
-    capacity: usize,
+    start: u64,
+    capacity: isize,
     interval: u64,
-    ticks: u64,
-    last_tick: u64,
-    correction: u64,
+    available: isize,
+    tick: u64,
+    quantum: usize,
     tx: mpsc::SyncSender<()>,
     rx: mpsc::Receiver<()>,
 }
 
 #[repr(C)]
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, PartialEq)]
 pub struct timespec {
     pub tv_sec: i64,
     pub tv_nsec: i64,
+}
+
+impl fmt::Debug for timespec {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "timespec(sec: {}, nsec: {})", self.tv_sec, self.tv_nsec)
+    }
 }
 
 impl Ratelimit {
@@ -33,17 +43,18 @@ impl Ratelimit {
     /// ```
     /// # use ratelimit::Ratelimit;
     ///
-    /// let mut r = Ratelimit::new(0, 1).unwrap();
-    pub fn new(capacity: usize, interval: u64) -> Option<Ratelimit> {
+    /// let mut r = Ratelimit::new(0, 0, 1, 1).unwrap();
+    pub fn new(capacity: usize, start: u64, interval: u64, quantum: usize) -> Option<Ratelimit> {
 
         let (tx, rx) = mpsc::sync_channel(capacity);
 
         Some(Ratelimit {
-            capacity: capacity,
+            start: start,
+            capacity: capacity as isize,
             interval: interval,
-            ticks: 0_u64,
-            last_tick: time::precise_time_ns(),
-            correction: 0_u64,
+            available: capacity as isize, // needs to go negative
+            quantum: quantum,
+            tick: 0_u64,
             tx: tx,
             rx: rx,
         })
@@ -51,8 +62,11 @@ impl Ratelimit {
 
     /// this should be run in a tight-loop in its own thread
     pub fn run(&mut self) {
-        self.block(1);
-        let _ = self.rx.recv();
+        let take = self.quantum;
+        self.block(take);
+        for _ in 0..take {
+            let _ = self.rx.recv();
+        }
     }
 
     /// give a sender for client to use
@@ -60,17 +74,42 @@ impl Ratelimit {
         self.tx.clone()
     }
 
-    /// block until can take
+    /// block as long as take says to
     fn block(&mut self, count: usize) {
-        loop {
-            self.tick();
-            if self.capacity >= count {
-                self.capacity -= count;
-                break;
-            }
-
-            self.clock_nanosleep(1, 0, &timespec { tv_sec: 0, tv_nsec: 1000 }, None);
+        //let delay = self.take(time::precise_time_ns(), count);
+        match self.take(time::precise_time_ns(), count) {
+            Some(ts) => {
+                let start = time::precise_time_ns();
+                self.clock_nanosleep(1, 0, &ts, None);
+                let stop = time::precise_time_ns();
+                let diff = (stop - start) as i64;
+                assert!(diff > ts.tv_nsec);
+            },
+            None => {},
         }
+    }
+
+    // return time to sleep until token is available
+    fn take(&mut self, time: u64, count: usize) -> Option<timespec> {
+
+        if count == 0 {
+            return None;
+        }
+
+        let _ = self.tick(time);
+        let available = self.available - count as isize;
+        if available >= 0 {
+            self.available = available;
+            return None;
+        }
+        let needed_ticks =
+            ((-1 * available + self.quantum as isize - 1) as f64 / (self.quantum as f64)) as u64;
+        let wait_time = needed_ticks * self.interval;
+        self.available = available;
+        let seconds = (wait_time as f64 / ONE_SECOND as f64).floor() as u64;
+        let nano_seconds = wait_time - seconds * ONE_SECOND;
+
+        Some(timespec { tv_sec: seconds as i64, tv_nsec: nano_seconds as i64 })
     }
 
     /// we need fine-grained sleep
@@ -94,24 +133,62 @@ impl Ratelimit {
     }
 
     /// move the time forward and do bookkeeping
-    fn tick(&mut self) {
-        let this_tick = time::precise_time_ns();
-        let interval = this_tick - self.last_tick;
+    fn tick(&mut self, now: u64) -> u64 {
+        //let tick: u64 = (now - self.start) / self.interval;
+        let tick: u64 = ((now - self.start) as f64 / self.interval as f64).floor() as u64;
 
-        if interval > self.interval {
-            let increment = (interval as f64 / self.interval as f64).floor() as u64;
-            self.correction += interval - (increment * self.interval);
-            self.ticks += increment;
-            self.capacity += increment as usize;
-            self.last_tick = this_tick;
+        if self.available >= self.capacity {
+            return tick;
         }
+        self.available += ((tick - self.tick) * self.quantum as u64) as isize;
+        if self.available > self.capacity {
+            self.available = self.capacity as isize;
+        }
+        self.tick = tick;
+        tick
+    }
+}
 
-        if self.correction > self.interval {
-            let increment = (self.correction as f64 / self.interval as f64).floor() as u64;
-            self.correction -= increment * self.interval;
-            self.ticks += increment;
-            self.capacity += increment as usize;
-            self.last_tick = this_tick;
-        }
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    extern crate time;
+
+    #[test]
+    fn test_tick_same() {
+        let mut r = Ratelimit::new(1, 0, 1000, 1).unwrap();
+
+        let time = time::precise_time_ns();
+        let tick = r.tick(time);
+        assert_eq!(tick, r.tick(time));
+    }
+
+    #[test]
+    fn test_tick_next() {
+        let mut r = Ratelimit::new(1, 0, 1000, 1).unwrap();
+
+        assert_eq!(r.tick(0), 0);
+        assert_eq!(r.tick(1), 0);
+        assert_eq!(r.tick(500), 0);
+        assert_eq!(r.tick(999), 0);
+        assert_eq!(r.tick(1000), 1);
+        assert_eq!(r.tick(1001), 1);
+        assert_eq!(r.tick(1999), 1);
+        assert_eq!(r.tick(2000), 2);
+        assert_eq!(r.tick(2001), 2);
+        assert_eq!(r.tick(2999), 2);
+    }
+
+    #[test]
+    fn test_take() {
+        let mut r = Ratelimit::new(1, 0, 1000, 1).unwrap();
+
+        assert_eq!(r.take(0, 1), None);
+        assert_eq!(r.take(0, 1), Some(timespec { tv_sec: 0, tv_nsec: 1000 }));
+        assert_eq!(r.take(0, 1), Some(timespec { tv_sec: 0, tv_nsec: 2000 }));
+        assert_eq!(r.take(1000, 1), Some(timespec { tv_sec: 0, tv_nsec: 2000 }));
+        assert_eq!(r.take(3000, 1), Some(timespec { tv_sec: 0, tv_nsec: 1000 }));
+        assert_eq!(r.take(5000, 1), None);
     }
 }
