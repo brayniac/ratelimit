fb4c01fd6c13e46940a59e4d7f429fb0e240b21a
Brian Martin <brayniac@gmail.com>
2016-08-05 16:30:01 +0000

    ratelimit 0.3.0
    - convert ratelimit to operate on std::time
    - update autodoc key

--- src/lib.rs
+++ src/lib.rs
@@ -21,29 +21,22 @@
 //!
 //! ```
 //!
-//! extern crate time;
 //! extern crate ratelimit;
 //!
 //! use std::thread;
 //! use std::sync::mpsc;
+//! use std::time::Duration;
 //!
-//! // create a Ratelimit
-//! let capacity = 1; // bucket can hold 10 tokens
-//! let start = time::precise_time_ns(); // start time
-//! let interval = 1_000_000_000; // 1 second => 1 Hz
-//! let quantum = 1; // 1 token per interval => 1 tokens/s
-//!
-//! let mut ratelimit = ratelimit::Ratelimit::new(
-//!     capacity,
-//!     start,
-//!     interval,
-//!     quantum
-//!     ).unwrap();
+//! let mut ratelimit = ratelimit::Ratelimit::configure()
+//!     .capacity(1) //number of tokens the bucket will hold
+//!     .quantum(1) //add one token per interval
+//!     .interval(Duration::new(1, 0)) //add quantum tokens every 1 second
+//!     .build();
 //!
 //! // count down to ignition
 //! println!("Count-down....");
-//! for i in 0..5 {
-//!     println!("T-Minus {}", (5 - i));
+//! for i in -10..0 {
+//!     println!("T {}", i);
 //!     ratelimit.block(1);
 //! }
 //! println!("Ignition!");
@@ -73,66 +66,173 @@
 
 #![crate_name = "ratelimit"]
 
-extern crate time;
 extern crate shuteye;
 
+use std::time::{Duration, Instant};
 use std::sync::mpsc;
-use shuteye::*;
+
+use shuteye::sleep;
+
+pub struct Config {
+    start: Instant,
+    capacity: u32,
+    quantum: u32,
+    interval: Duration,
+}
 
 pub struct Ratelimit {
-    start: u64,
-    last_tick: u64,
-    capacity: isize,
-    interval: u64,
-    available: isize,
+    config: Config,
+    t0: Instant,
+    available: i64,
     tick: u64,
-    quantum: usize,
     tx: mpsc::SyncSender<()>,
     rx: mpsc::Receiver<()>,
 }
 
-impl Ratelimit {
-    /// create a new ratelimit instance
+impl Config {
+    /// returns a Ratelimit from the Config
     ///
     /// # Example
     /// ```
-    /// # use ratelimit::*;
+    /// # use ratelimit::Ratelimit;
     ///
-    /// let mut r = Ratelimit::new(0, 0, 1, 1).unwrap();
-    pub fn new(capacity: usize, start: u64, interval: u64, quantum: usize) -> Option<Ratelimit> {
+    /// let mut r = Ratelimit::configure().build();
+    pub fn build(self) -> Ratelimit {
+        Ratelimit::configured(self)
+    }
 
-        let (tx, rx) = mpsc::sync_channel(capacity);
+    /// sets the number of tokens to add per interval
+    ///
+    /// # Example
+    /// ```
+    /// # use ratelimit::Ratelimit;
+    ///
+    /// let mut r = Ratelimit::configure()
+    ///     .quantum(100)
+    ///     .build();
+    pub fn quantum(mut self, quantum: u32) -> Self {
+        self.quantum = quantum;
+        self
+    }
 
-        Some(Ratelimit {
-            start: start,
-            capacity: capacity as isize,
-            interval: interval,
-            available: 1,
-            quantum: quantum,
-            last_tick: start,
-            tick: 0_u64,
+    /// sets the bucket capacity
+    ///
+    /// # Example
+    /// ```
+    /// # use ratelimit::Ratelimit;
+    ///
+    /// let mut r = Ratelimit::configure()
+    ///     .capacity(100)
+    ///     .build();
+    pub fn capacity(mut self, capacity: u32) -> Self {
+        self.capacity = capacity;
+        self
+    }
+
+    /// set the duration between token adds
+    ///
+    /// # Example
+    /// ```
+    /// # use ratelimit::Ratelimit;
+    /// # use std::time::Duration;
+    ///
+    /// let mut r = Ratelimit::configure()
+    ///     .interval(Duration::new(2, 0))
+    ///     .build();
+    pub fn interval(mut self, interval: Duration) -> Self {
+        self.interval = interval;
+        self
+    }
+
+    /// set the frequency in Hz of the ratelimiter
+    ///
+    /// # Example
+    /// ```
+    /// # use ratelimit::Ratelimit;
+    ///
+    /// let mut rate = 100_000; // 100kHz
+    /// let mut r = Ratelimit::configure()
+    ///     .frequency(rate)
+    ///     .build();
+    pub fn frequency(mut self, cycles: u32) -> Self {
+        let mut interval = Duration::new(1, 0);
+        interval /= cycles;
+        self.interval = interval;
+        self
+    }
+}
+
+impl Default for Config {
+    fn default() -> Config {
+        Config {
+            start: Instant::now(),
+            capacity: 1,
+            quantum: 1,
+            interval: Duration::new(1, 0),
+        }
+    }
+}
+
+impl Default for Ratelimit {
+    fn default() -> Ratelimit {
+        Config::default().build()
+    }
+}
+
+impl Ratelimit {
+    /// create a new default ratelimit instance
+    ///
+    /// # Example
+    /// ```
+    /// # use ratelimit::Ratelimit;
+    ///
+    /// let mut r = Ratelimit::new();
+    pub fn new() -> Ratelimit {
+        Default::default()
+    }
+
+    /// create a new `Config` for building a custom `Ratelimit`
+    ///
+    /// # Example
+    /// ```
+    /// # use ratelimit::Ratelimit;
+    ///
+    /// let mut r = Ratelimit::configure().build();
+    pub fn configure() -> Config {
+        Config::default()
+    }
+
+    // internal function for building a Ratelimit from its Config
+    fn configured(config: Config) -> Ratelimit {
+        let (tx, rx) = mpsc::sync_channel(config.capacity as usize);
+        let t0 = config.start;
+        Ratelimit {
+            config: config,
+            t0: t0,
+            available: 0,
+            tick: 0,
             tx: tx,
             rx: rx,
-        })
+        }
     }
 
     /// run the ratelimiter
     ///
     /// # Example
     /// ```
-    /// # use ratelimit::*;
+    /// # use ratelimit::Ratelimit;
     ///
-    /// let mut r = Ratelimit::new(1, 0, 1, 1).unwrap();
+    /// let mut r = Ratelimit::new();
     ///
     /// let sender = r.clone_sender();
     /// let _ = sender.try_send(());
     ///
     /// r.run(); // invoke in a tight-loop in its own thread
     pub fn run(&mut self) {
-        let take = self.quantum;
-        self.block(take);
+        let quantum = self.config.quantum;
+        self.block(quantum);
         let mut unused = 0;
-        for _ in 0..take {
+        for _ in 0..self.config.quantum {
             match self.rx.try_recv() {
                 Ok(..) => {}
                 Err(_) => {
@@ -149,7 +249,7 @@ impl Ratelimit {
     /// ```
     /// # use ratelimit::*;
     ///
-    /// let mut r = Ratelimit::new(1, 0, 1, 1).unwrap();
+    /// let mut r = Ratelimit::new();
     ///
     /// let sender = r.clone_sender();
     ///
@@ -165,130 +265,125 @@ impl Ratelimit {
         self.tx.clone()
     }
 
-    // block as long as take says to
-    pub fn block(&mut self, count: usize) {
-        if self.interval == 0 {
+    /// this call is the blocking API of Ratelimit
+    ///
+    /// # Example
+    /// ```
+    /// # use ratelimit::Ratelimit;
+    ///
+    /// let mut r = Ratelimit::new();
+    /// for i in -10..0 {
+    ///     println!("T {}...", i);
+    ///     r.block(1);
+    /// }
+    /// println!("Ignition!");
+    pub fn block(&mut self, count: u32) {
+        if self.config.interval == Duration::new(0, 0) {
             return;
         }
-        if let Some(ts) = self.take(time::precise_time_ns(), count) {
-            let _ = shuteye::sleep(ts);
+        if let Some(ts) = self.take(Instant::now(), count) {
+            let _ = sleep(ts);
         }
     }
 
-    fn give(&mut self, count: usize) {
-        self.available += count as isize;
+    // internal function to give back unused tokens
+    fn give(&mut self, count: u32) {
+        self.available += count as i64;
 
-        if self.available >= self.capacity {
-            self.available = self.capacity;
+        if self.available >= self.config.capacity as i64 {
+            self.available = self.config.capacity as i64;
         }
     }
 
-    // return time to sleep until token is available
-    fn take(&mut self, time: u64, count: usize) -> Option<Timespec> {
-        if count == 0 {
+    // return time to sleep until tokens are available
+    fn take(&mut self, t1: Instant, tokens: u32) -> Option<Duration> {
+        if tokens == 0 {
             return None;
         }
 
-        let _ = self.tick(time);
-        let available = self.available - count as isize;
+        let _ = self.tick(t1);
+        let available = self.available - tokens as i64;
         if available >= 0 {
             self.available = available;
             return None;
         }
-        let needed_ticks = ((-1 * available + self.quantum as isize - 1) as f64 /
-                            (self.quantum as f64)) as u64;
-        let wait_time = needed_ticks * self.interval - (time - self.last_tick);
-        self.available = available;
-        match Timespec::from_nano(wait_time as i64) {
-            Err(_) => {
-                panic!("error getting Timespec from wait_time");
-            }
-            Ok(ts) => Some(ts),
+
+        let needed_ticks = -available as u32 / self.config.quantum;
+        let mut wait_time = self.config.interval * needed_ticks;
+        if t1 > self.t0 {
+            wait_time -= t1 - self.t0;
         }
+        self.available = available;
+        Some(wait_time)
     }
 
     // move the time forward and do bookkeeping
-    fn tick(&mut self, now: u64) -> u64 {
-        let tick: u64 = ((now - self.start) as f64 / self.interval as f64).floor() as u64;
+    fn tick(&mut self, t1: Instant) -> u64 {
+        let tick = cycles(t1.duration_since(self.t0), self.config.interval) as u64 + self.tick;
 
-        if self.available >= self.capacity {
+        if self.available >= self.config.capacity as i64 {
             return tick;
         }
         if tick == self.tick {
             return tick;
         }
 
-        self.available += ((tick - self.tick) * self.quantum as u64) as isize;
-        if self.available > self.capacity {
-            self.available = self.capacity as isize;
+        self.available += (tick as i64 - self.tick as i64) * self.config.quantum as i64;
+        if self.available > self.config.capacity as i64 {
+            self.available = self.config.capacity as i64;
         }
         self.tick = tick;
-        self.last_tick = now;
+        self.t0 = t1;
         tick
     }
+}
+
+// returns the number of cycles of period length within the duration
+fn cycles(duration: Duration, period: Duration) -> f64 {
+    let d = 1_000_000_000 * duration.as_secs() as u64 + duration.subsec_nanos() as u64;
+    let p = 1_000_000_000 * period.as_secs() as u64 + period.subsec_nanos() as u64;
+    d as f64 / p as f64
 }
 
 #[cfg(test)]
 mod tests {
     use super::Ratelimit;
+    use std::time::Duration;
 
-    extern crate time;
     extern crate shuteye;
 
     #[test]
     fn test_tick_same() {
-        let mut r = Ratelimit::new(1, 0, 1000, 1).unwrap();
+        let mut r = Ratelimit::new();
+        let t0 = r.t0;
 
-        let time = time::precise_time_ns();
-        let tick = r.tick(time);
-        assert_eq!(tick, r.tick(time));
+        let tick = r.tick(t0);
+        assert_eq!(tick, r.tick(t0));
     }
 
     #[test]
     fn test_tick_next() {
-        let mut r = Ratelimit::new(1, 0, 1000, 1).unwrap();
+        let mut r = Ratelimit::new();
+        let t0 = r.t0;
 
-        assert_eq!(r.tick(0), 0);
-        assert_eq!(r.tick(1), 0);
-        assert_eq!(r.tick(500), 0);
-        assert_eq!(r.tick(999), 0);
-        assert_eq!(r.tick(1000), 1);
-        assert_eq!(r.tick(1001), 1);
-        assert_eq!(r.tick(1999), 1);
-        assert_eq!(r.tick(2000), 2);
-        assert_eq!(r.tick(2001), 2);
-        assert_eq!(r.tick(2999), 2);
+        assert_eq!(r.tick(t0), 0);
+        assert_eq!(r.tick(t0 + Duration::new(0, 1)), 0);
+        assert_eq!(r.tick(t0 + Duration::new(0, 999_999_999)), 0);
+        assert_eq!(r.tick(t0 + Duration::new(1, 0)), 1);
+        assert_eq!(r.tick(t0 + Duration::new(1, 1)), 1);
+        assert_eq!(r.tick(t0 + Duration::new(1, 999_999_999)), 1);
+        assert_eq!(r.tick(t0 + Duration::new(2, 0)), 2);
     }
 
     #[test]
-    fn test_take_0() {
-        let mut r = Ratelimit::new(1, 0, 1000, 1).unwrap();
+    fn test_take() {
+        let mut r = Ratelimit::new();
+        let mut t = r.t0;
+        t += Duration::new(1, 0);
 
-        assert_eq!(r.take(0, 1), None);
-        assert_eq!(r.take(0, 1).unwrap().as_nsec(), 1000);
-        assert_eq!(r.take(0, 1).unwrap().as_nsec(), 2000);
-        assert_eq!(r.take(1000, 1).unwrap().as_nsec(), 2000);
-        assert_eq!(r.take(3000, 1).unwrap().as_nsec(), 1000);
-        assert_eq!(r.take(5000, 1), None);
-    }
+        assert_eq!(r.take(t, 1), None);
 
-    #[test]
-    fn test_take_1() {
-        let mut r = Ratelimit::new(100, 0, 1000, 1).unwrap();
-
-        assert_eq!(r.take(0, 1), None);
-        assert_eq!(r.take(0, 1).unwrap().as_nsec(), 1000);
-        assert_eq!(r.take(0, 1).unwrap().as_nsec(), 2000);
-        assert_eq!(r.take(1000, 1).unwrap().as_nsec(), 2000);
-        assert_eq!(r.take(3000, 1).unwrap().as_nsec(), 1000);
-        assert_eq!(r.take(5000, 1), None);
-    }
-
-    #[test]
-    fn test_take_2() {
-        let mut r = Ratelimit::new(1, 0, 1000, 1).unwrap();
-
-        assert_eq!(r.take(0, 1), None);
-        assert_eq!(r.take(1, 1).unwrap().as_nsec(), 999);
+        t += Duration::new(0, 1);
+        assert_eq!(r.take(t, 1).unwrap().subsec_nanos(), 999_999_999);
     }
 }
